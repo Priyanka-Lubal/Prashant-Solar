aee0809b02c11873d04689625d9466d079d86c7c
Priyanka-Lubal <priyanka.lubal33@gmail.com>
2017-03-12 19:34:31 +0530

    header changed

--- site/includes/popup.php
+++ site/includes/popup.php
@@ -10,19 +10,33 @@
 <link href="css/elements.css" rel="stylesheet">
 <script src="js/my_js.js"></script>
 
-<div class="" id="abc">
+<div id="abc">
     <!-- Popup Div Starts Here -->
     <div id="popupContact">
         <!-- Contact Us Form -->
         <form action="#" id="form1" method="post" name="form">
-            <img id="close" src="images/3.png" onclick="div_hide()">
+            <img id="close" src="img/close-128.png" height="25px" onclick="div_hide()">
             <h2>Calculator</h2>
             <hr>
-            <strong>Enter Rooftop area</strong>
-            <input id="name" name="name" placeholder="Enter in squaremeter" type="text"><br>
-            <a href="calculator.php" id="submit1">Submit</a>
+            <strong>Enter Rooftop area</strong></><br><br>
+            <input id="name" name="name" placeholder="Enter in squaremeter" type="number" onchange="save()"><br><br>
+            <a href="#" id="submit1" onclick="check_empty()">Submit</a>
         </form>
     </div>
     <!-- Popup Div Ends Here -->
 </div>
 
+<div id="xyz">
+    <!-- Popup Div Starts Here -->
+    <div id="popupContact">
+        <!-- Contact Us Form -->
+        <form action="#" id="form1" method="post" name="form">
+            <img id="close" src="img/close-128.png" height="25px" onclick="hide()">
+            <h2>Calculator</h2>
+            <hr>
+            <label>Your Input is:</label>
+            <p><span id="display"></span></p>
+        </form>
+    </div>
+    <!-- Popup Div Ends Here -->
+</div>
--- site/js/my_js.js
+++ site/js/my_js.js
@@ -2,19 +2,40 @@
  * Created by acer on 3/8/2017.
  */
 // Validating Empty Field
+function showinput() {
+    document.getElementById('display').innerHTML =
+        document.getElementById('name').value;
+}
+
+function submit() {
+    document.getElementById('abc').style.display = "none";
+    document.getElementById('xyz').style.display = "block";
+    showinput();
+}
+
 function check_empty() {
-    if (document.getElementById('name').value == "" || document.getElementById('email').value == "" || document.getElementById('msg').value == "") {
+    if (document.getElementById('name').value == "") {
         alert("Fill All Fields !");
     } else {
-        document.getElementById('form').submit();
-        alert("Form Submitted Successfully...");
+        submit();
     }
 }
 //Function To Display Popup
 function popup() {
     document.getElementById('abc').style.display = "block";
 }
+
+
 //Function to Hide Popup
 function div_hide(){
     document.getElementById('abc').style.display = "none";
+}
+
+function hide() {
+    document.getElementById('xyz').style.display = "none";
+ }
+
+function save() {
+    var num;
+    num = document.getElementById('name').value;
 }
