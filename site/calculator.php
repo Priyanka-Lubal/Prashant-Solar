193dc23575eba3d6cee6d95595a9dd00484867ce
meetg <meetgopani1996@gmail.com>
2017-03-14 22:30:06 +0530

    form done, add script

--- site/calculator.php
+++ site/calculator.php
@@ -48,15 +48,192 @@
                 <hr>
             </div>
             <div class="col-lg-12 text-center">
-                <img class="img-responsive img-border img-full" src="img/1.jpg" alt="">
-                <h2>Post Title
+                <img class="img-responsive img-border img-full" src="img/1.jpg" alt="hello">
+                <h2>Calculate the Load
                     <br>
-                    <small>October 13, 2013</small>
                 </h2>
-                <p>Lid est laborum dolo rumes fugats untras. Etharums ser quidem rerum facilis dolores nemis omnis
-                    fugats vitaes nemo minima rerums unsers sadips amets. Sed ut perspiciatis unde omnis iste natus
-                    error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo
-                    inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+                <script>
+
+                    function calc_load() {
+
+                        total_load = 0;
+                        for (i = 0; i <= 20; i++) {
+
+                            total_load = total_load + (Number($('#qty_' + i).val()) * Number($('#watt_' + i).val()));
+
+                        }
+
+                        $('#total_load').val(total_load);
+                    }
+                </script>
+                <form method="post" action="calculator.php?action=method2_process" name="form1" id="form1">
+                    <table class="table table-condensed" style="max-width:600px;">
+                        <tr>
+                            <th>Load</th>
+                            <th>Watt</th>
+                            <th>Quantity</th>
+                        </tr>
+                        <tr>
+                            <td>4' TubeLight LED</td>
+                            <td>15<input type="hidden" name="watt_0" id="watt_0" value="15"></td>
+                            <td><input type="text" name="watt_0" id="watt_0" value="0" class="form-control"
+                                       style="max-width:70px; max-height:30px; ">
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>2' TubeLight LED</td>
+                            <td>8<input type="hidden" name="watt_1" id="watt_1" value="8"></td>
+                            <td><input type="text" name="watt_1" id="watt_1" value="0" class="form-control"
+                                       style="max-width:70px; max-height:30px; ">
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>4' TubeLight CFL</td>
+                            <td>60<input type="hidden" name="watt_2" id="watt_2" value="60"></td>
+                            <td><input type="text" name="watt_2" id="watt_2" value="0" class="form-control"
+                                       style="max-width:70px; max-height:30px; ">
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>2' TubeLight CFL</td>
+                            <td>30<input type="hidden" name="watt_3" id="watt_3" value="30"></td>
+                            <td><input type="text" name="watt_3" id="watt_3" value="0" class="form-control"
+                                       style="max-width:70px; max-height:30px; ">
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Ceiling Fan</td>
+                            <td>75<input type="hidden" name="watt_4" id="watt_4" value="75"></td>
+                            <td><input type="text" name="watt_4" id="watt_4" value="0" class="form-control"
+                                       style="max-width:70px; max-height:30px; ">
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Table Fan</td>
+                            <td>40<input type="hidden" name="watt_5" id="watt_5" value="40"></td>
+                            <td><input type="text" name="watt_5" id="watt_5" value="0" class="form-control"
+                                       style="max-width:70px; max-height:30px; ">
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Pedestal Fan</td>
+                            <td>100<input type="hidden" name="watt_6" id="watt_6" value="100"></td>
+                            <td><input type="text" name="watt_6" id="watt_6" value="0" class="form-control"
+                                       style="max-width:70px; max-height:30px; ">
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Exhaust Fan</td>
+                            <td>250<input type="hidden" name="watt_7" id="watt_7" value="250"></td>
+                            <td><input type="text" name="watt_7" id="watt_7" value="0" class="form-control"
+                                       style="max-width:70px; max-height:30px; ">
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Refrigerator 165 LTR</td>
+                            <td>100<input type="hidden" name="watt_8" id="watt_8" value="100"></td>
+                            <td><input type="text" name="watt_8" id="watt_8" value="0" class="form-control"
+                                       style="max-width:70px; max-height:30px; ">
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Refrigerator 310 LTR</td>
+                            <td>400<input type="hidden" name="watt_9" id="watt_9" value="400"></td>
+                            <td><input type="text" name="watt_9" id="watt_9" value="0" class="form-control"
+                                       style="max-width:70px; max-height:30px; ">
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>CD/DVD</td>
+                            <td>30<input type="hidden" name="watt_10" id="watt_10" value="30"></td>
+                            <td><input type="text" name="watt_10" id="watt_10" value="0" class="form-control"
+                                       style="max-width:70px; max-height:30px; ">
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Printer</td>
+                            <td>25<input type="hidden" name="watt_11" id="watt_11" value="25"></td>
+                            <td><input type="text" name="watt_11" id="watt_11" value="0" class="form-control"
+                                       style="max-width:70px; max-height:30px; ">
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Computrer</td>
+                            <td>120<input type="hidden" name="watt_12" id="watt_12" value="120"></td>
+                            <td><input type="text" name="watt_12" id="watt_12" value="0" class="form-control"
+                                       style="max-width:70px; max-height:30px; ">
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Fax</td>
+                            <td>250<input type="hidden" name="watt_13" id="watt_13" value="250"></td>
+                            <td><input type="text" name="watt_13" id="watt_13" value="0" class="form-control"
+                                       style="max-width:70px; max-height:30px; ">
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Water Pump-0.5 HP</td>
+                            <td>375<input type="hidden" name="watt_14" id="watt_14" value="375"></td>
+                            <td><input type="text" name="watt_14" id="watt_14" value="0" class="form-control"
+                                       style="max-width:70px; max-height:30px; ">
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Air cooler-Small</td>
+                            <td>250<input type="hidden" name="watt_15" id="watt_15" value="250"></td>
+                            <td><input type="text" name="watt_15" id="watt_15" value="0" class="form-control"
+                                       style="max-width:70px; max-height:30px; ">
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Air cooler-Big</td>
+                            <td>400<input type="hidden" name="watt_16" id="watt_16" value="400"></td>
+                            <td><input type="text" name="watt_16" id="watt_16" value="0" class="form-control"
+                                       style="max-width:70px; max-height:30px; ">
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>AC-1 ton</td>
+                            <td>1400<input type="hidden" name="watt_17" id="watt_17" value="1400"></td>
+                            <td><input type="text" name="watt_17" id="watt_17" value="0" class="form-control"
+                                       style="max-width:70px; max-height:30px; ">
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>AC-1.5 ton</td>
+                            <td>2100<input type="hidden" name="watt_18" id="watt_18" value="2100"></td>
+                            <td><input type="text" name="watt_18" id="watt_18" value="0" class="form-control"
+                                       style="max-width:70px; max-height:30px; ">
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Vacuum Cleaner</td>
+                            <td>500<input type="hidden" name="watt_19" id="watt_19" value="500"></td>
+                            <td><input type="text" name="watt_19" id="watt_19" value="0" class="form-control"
+                                       style="max-width:70px; max-height:30px; ">
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Other</td>
+                            <td><input type="text" name="watt_20" id="watt_20" value="0" class="form-control"
+                                       style="max-width:70px; max-height:30px; ">
+                            </td>
+                            <td><input type="text" name="watt_20" id="watt_20" value="0" class="form-control"
+                                       style="max-width:70px; max-height:30px; ">
+                            </td>
+                        </tr>
+                        <tr>
+                            <td><b>Total Load</b></td>
+                            <td>
+                                <!-- <input type="button" class="btn btn-sm btn-default" onclick="javascript:calc_load();" value="Recalculate Load"> --></td>
+                            <td><input type="text" name="total_load" id="total_load" value="0" class="form-control"
+                                       style="max-width:100px; max-height:30px;">
+                                <div class="error_holder"></div>
+                            </td>
+                        </tr>
+                    </table>
+                    <p><input type="submit" class="btn btn-primary" value="Submit"/></p>
+                </form>
                 <a href="#" class="btn btn-default btn-lg">Read More</a>
                 <hr>
             </div>
