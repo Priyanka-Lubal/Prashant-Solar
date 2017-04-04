8f41b78b3517ee42b009a8f7e1ea6297cb10d137
meetg <meetgopani1996@gmail.com>
2017-04-04 13:38:00 +0530

    footer appears everywhere

--- site/form-handler-nodb.php
+++ site/form-handler-nodb.php
@@ -17,7 +17,7 @@ $mail = new PHPMailer;
     $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
     $mail->SMTPAuth = true;                               // Enable SMTP authentication
     $mail->Username = 'meetgopani1996@gmail.com';                 // SMTP username
-    $mail->Password = 'meet311296';                           // SMTP password
+    $mail->Password = 'password';                           // SMTP password
     $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
     $mail->Port = 587;                                    // TCP port to connect to
 
