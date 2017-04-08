f60db83888cb22d0e80b3d0319f6dc0d887176b2
meetg <meetgopani1996@gmail.com>
2017-04-08 14:52:09 +0530

    > removed

--- site/includes/popup.php
+++ site/includes/popup.php
@@ -18,9 +18,10 @@
             <img id="close" src="img/close-128.png" height="25px" onclick="div_hide()">
             <h2>Calculator</h2>
             <hr>
-            <strong>Enter Rooftop area</strong></><br><br>
-            <input id="name" name="name" placeholder="Enter in square-feet" type="number" onchange="save()"><br><br>
-            <a href="#" id="submit1" onclick="check_empty()">Submit</a>
+            <strong>Enter Rooftop area</strong></
+        ><br><br>
+        <input id="name" name="name" placeholder="Enter in square-feet" type="number" onchange="save()"><br><br>
+        <a href="#" id="submit1" onclick="check_empty()">Submit</a>
         </form>
     </div>
     <!-- Popup Div Ends Here -->
@@ -37,9 +38,10 @@
             <h3>Calculator</h3>
             <hr>
             <h1>Congratulations!!!</h1>
-            <strong>You can save upto <span id="unit"></span> units per month by installing a <span id="solar"></span> KWP solar rooftop system on <span id="display"></span>  sq.ft of roof space.<br><br></strong>>
+            <strong>You can save upto <span id="unit"></span> units per month by installing a <span id="solar"></span>
+                KWP solar rooftop system on <span id="display"></span> sq.ft of roof space.<br><br></strong>
             <a href="calculator.php" id="submit1">To know more</a><br>
-                <a href="#" onclick="popup()"><u>Re-Enter Rooftop area</u></a>
+            <a href="#" onclick="popup()"><u>Re-Enter Rooftop area</u></a>
         </form>
     </div>
     <!-- Popup Div Ends Here -->
