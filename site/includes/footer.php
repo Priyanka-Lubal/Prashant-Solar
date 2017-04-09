b96c982cc6938e360dba872c665d00ee4db46551
meetg <meetgopani1996@gmail.com>
2017-04-09 21:17:20 +0530

    maps location added

--- site/includes/footer.php
+++ site/includes/footer.php
@@ -24,8 +24,13 @@
             </div>
             <div class="footer-center col-md-4 col-sm-6">
                 <div>
-                    <i class="fa fa-map-marker"></i>
-                    <p>Maharashtra, India</p>
+                    <a href="https://www.google.co.in/maps/place/PRASHANT+SOLAR/@19.1842094,73.1919258,15z/data=!4m5!3m4!1s0x0:0xc7fd7e4f3cce8d3d!8m2!3d19.1842094!4d73.1919258?sa=X&ved=0ahUKEwj5hc-E2JfTAhXGLY8KHdmBCPMQ_BIIYDAO"
+                       target="_blank">
+                        <i class="fa fa-map-marker"></i></a>
+                    <p>
+                        <a href="https://www.google.co.in/maps/place/PRASHANT+SOLAR/@19.1842094,73.1919258,15z/data=!4m5!3m4!1s0x0:0xc7fd7e4f3cce8d3d!8m2!3d19.1842094!4d73.1919258?sa=X&ved=0ahUKEwj5hc-E2JfTAhXGLY8KHdmBCPMQ_BIIYDAO"
+                           target="_blank">
+                            Maharashtra, India</a></p>
                 </div>
                 <div>
                     <a href="tel:+91 79777 47687​⁠​"><i class="fa fa-phone"></i></a>
